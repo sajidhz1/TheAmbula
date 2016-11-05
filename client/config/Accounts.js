45e694ea9675f9cce193012ab4072c75efdeccff
sajidhz1 <sajidhzazahir@gmail.com>
2016-11-05 19:54:18 +0530

    side nav bar updated with light colors

--- client/config/Accounts.js
+++ client/config/Accounts.js
@@ -1,3 +1,37 @@
 Accounts.ui.config({
-    passwordSignupFields : "USERNAME_AND_EMAIL"
+    passwordSignupFields : "USERNAME_AND_EMAIL",
+    extraSignupFields: [{
+        fieldName: 'first-name',
+        fieldLabel: 'First name',
+        inputType: 'text',
+        visible: true,
+        validate: function(value, errorFunction) {
+          if (!value) {
+            errorFunction("Please write your first name");
+            return false;
+          } else {
+            return true;
+          }
+        }
+    }, {
+        fieldName: 'last-name',
+        fieldLabel: 'Last name',
+        inputType: 'text',
+        visible: true,
+    },  {
+        fieldName: 'terms',
+        fieldLabel: 'I accept the terms and conditions',
+        inputType: 'checkbox',
+        visible: true,
+        saveToProfile: false,
+        validate: function(value, errorFunction) {
+            if (value) {
+                return true;
+            } else {
+                errorFunction('You must accept the terms and conditions.');
+                return false;
+            }
+        }
+    }]
 });
+
