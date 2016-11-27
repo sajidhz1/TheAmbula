b5c4f842c1e81d5f5c5bbb9ade30cf39930b4bfc
dulitharuvin <dulitha.ruvin@gmail.com>
2016-11-27 22:59:47 +0530

    home youtube card view user firstname ,lastname added

--- server/publishers.js
+++ server/publishers.js
@@ -1,16 +1,23 @@
+import {Meteor} from 'meteor/meteor';
+
 import {YoutubeVideos} from '../imports/api/youtubevideos.js';
 //
-Meteor.publish("search-videos", function(query){
-  check(query, String);
+Meteor.publish("search-videos", function (query) {
+    check(query, String);
 
-  return YoutubeVideos.find({videoTitle: {$regex: query, $options: '-i'}});
+    return YoutubeVideos.find({videoTitle: {$regex: query, $options: '-i'}});
 });
 
 
-Meteor.publish("search-videos-by-owner", function(owner){
-  check(owner, String);
+Meteor.publish("search-videos-by-owner", function (owner) {
+    check(owner, String);
 
-  return YoutubeVideos.find({owner : owner});
+    return YoutubeVideos.find({owner: owner});
 });
+
+Meteor.publish("get-user-by-id", function () {
+    return Meteor.users.find({}, {fields: {profile: 1}});
+});
+
 
 
