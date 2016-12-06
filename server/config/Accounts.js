b98e06ed8243fbed32ea5831cf31679e059d999b
sajidhz1 <sajidhzazahir@gmail.com>
2016-12-06 22:46:57 +0530

    password reset

--- server/config/Accounts.js
+++ server/config/Accounts.js
@@ -2,3 +2,20 @@ Accounts.config({
     sendVerificationEmail: true, 
     forbidClientAccountCreation: false
 });
+
+// Accounts.emailTemplates.siteName = "TheAmbula";
+// Accounts.emailTemplates.from     = "GoDunk <admin@godunk.com>";
+
+Accounts.emailTemplates.verifyEmail = {
+  subject() {
+    return "[GoDunk] Verify Your Email Address";
+  },
+  text( user, url ) {
+    let emailAddress   = user.emails[0].address,
+        urlWithoutHash = url.replace( '#/', '' ),
+        supportEmail   = "support@theambula.com",
+        emailBody      = `To verify your email address (${emailAddress}) visit the following link:\n\n${urlWithoutHash}\n\n If you did not request this verification, please ignore this email. If you feel something is wrong, please contact our support team: ${supportEmail}.`;
+
+    return emailBody;
+  }
+};
---
+++ server/smtp.js
@@ -0,0 +1,3 @@
+Meteor.startup(function () {
+process.env.MAIL_URL = 'smtp://app58669865@heroku.com:ijkklg6m1991@smtp.sendgrid.net:587';
+});
