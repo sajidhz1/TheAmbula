38c44b664eb451ba8b79a0af6bd885a3775ce07a
sajidhz1 <sajidhzazahir@gmail.com>
2016-12-30 23:16:00 +0530

    Share button app ID

--- client/config/ShareIt.js
+++ client/config/ShareIt.js
@@ -1,7 +1,7 @@
  ShareIt.configure({
         sites: {
             'facebook': {
-                'appId': process.env.FACEBOOK_APPID
+                'appId': '753864634705819'
             }
         }
     });
