0ff512eeb3d0330e22bcf6b46df38174315f6122
dulitharuvin <dulitha.ruvin@gmail.com>
2017-03-11 01:46:59 +0530

    Featured video list added to single video view

---
+++ imports/ui/featuredvideotile/featuredvideotile.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Dulitha RD on 3/10/2017.
+ */
+import {Meteor} from 'meteor/meteor';
+import {Template} from 'meteor/templating';
+
+import './featuredvideotile.html';
+
+Template.featuredVideoTile.onCreated(function bodyOnCreated() {
+    
+});
+
+Template.featuredVideoTile.events({
+
+});
+
+
+//TODO : handle profile code using the profile object
+Template.featuredVideoTile.helpers({
+
+});
