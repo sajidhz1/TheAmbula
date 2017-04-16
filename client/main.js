22b391c088f21f4bb786a42854876647e106e240
dulitharuvin <dulitha.ruvin@gmail.com>
2017-04-16 09:53:31 +0530

    Article saving to the DB is done

--- client/main.js
+++ client/main.js
@@ -7,7 +7,7 @@ import '../imports/ui/heartbutton/heartbutton';
 import '../imports/ui/youtubevideoupdate/youtubevideoupdateform.js';
 import '../imports/ui/singlepoststatsview/singlepoststatview.js';
 import '../imports/ui/featuredvideotile/featuredvideotile.js';
-
+import '../imports/ui/article/articleaddform.js';
 import '../imports/ui/confirmation_dialog/recipedeleteconfirmbox.js';
 import '../imports/ui/report_dialog/recipereportdialogbox.js';
 
---
+++ imports/api/article.js
@@ -0,0 +1,114 @@
+/**
+ * Created by Dulitha RD on 4/14/2017.
+ */
+import {Meteor} from 'meteor/meteor';
+import {Mongo} from 'meteor/mongo';
+import {check} from 'meteor/check';
+
+export const Articles = new Mongo.Collection('articles');
+
+var Schemas = {};
+
+Schemas.article = new SimpleSchema({
+    articleTitle: {
+        type: String,
+        label: "Article title",
+        max: 250,
+        min: 10
+    },
+    articleBody: {
+        type: String,
+        label: "Article Body",
+        max: 10000,
+        min: 1000
+    },
+    articleType:{
+        type: String,
+        label: 'Article Type',
+    },
+    createdAt: {
+        type: Date,
+        label: "Date video was saved",
+    },
+    owner: {
+        type: String,
+        label: "User who saved the article"
+    },
+    username: {
+        type: String,
+        label: "Username of who saved the article",
+        optional: true
+    },
+    updatedAt: {
+        type: Date,
+        label: "Date article details were updated",
+        optional: true
+    }
+});
+
+Articles.attachSchema(Schemas.article);
+
+Meteor.methods({
+    'articles.insert': function (newArticle) {
+
+        if (!this.userId) {
+            throw new Meteor.Error('not-authorized');
+        }
+
+        newArticle.owner = this.userId;
+        newArticle.username = Meteor.users.findOne(this.userId).username;
+
+        return Articles.insert(newArticle, {validationContext: 'insertForm'});
+
+    },
+
+    'articles.delete': function (articleId) {
+
+        check(articleId, String);
+
+        if (Meteor.isServer) {
+            if (!this.userId) {
+                throw new Meteor.Error('not-authorized');
+            }
+
+            const article = Articles.findOne(articleId);
+
+            if (article.owner !== this.userId) {
+                throw new Meteor.Error('not-authorized');
+            } else {
+                Articles.remove(articleId);
+            }
+        }
+    },
+
+    'articles.update': function (articleId, articleTitle, articleType, articleBody) {
+
+        check(articleId, String);
+        check(articleTitle, String);
+        check(articleType, String);
+        check(articleBody, String);
+
+
+        const article = Articles.findOne(articleId);
+
+        // Make sure only the task owner can make a task private
+        if (article.owner !== this.userId) {
+            throw new Meteor.Error('not-authorized');
+        }
+
+        return Articles.update(articleId, {
+            $set: {
+                articleTitle: articleTitle,
+                articleType: articleType,
+                articleBody: articleBody,
+                updatedAt: new Date()
+            }
+        });
+
+    },
+
+    'articles.user': function (ytVideoID) { //  or try saving post ownerID in a Session
+        check(ytVideoID, String);
+        return Articles.findOne(ytVideoID).owner;
+    }
+});
