6e4a6c051d8ad650e417ef2d54017b59fa19b9ac
dulitharuvin <dulitha.ruvin@gmail.com>
2017-04-30 21:55:39 +0530

    ImageUpload.js removed from tracking tree

--- client/config/ImageUpload.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Dulitha RD on 4/30/2017.
- */
-$.cloudinary.config({
-    cloud_name: "hwshbhsyq"
-});
--- server/config/ImageUpload.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Dulitha RD on 4/30/2017.
- */
-Cloudinary.config({
-    cloud_name: 'hwshbhsyq',
-    api_key: '765487893716384',
-    api_secret: 'rYsjtZYYZGWa9hT_6mF-9BSZ36I'
-});
