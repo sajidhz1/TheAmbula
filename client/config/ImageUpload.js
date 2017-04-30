845219a224d238ab01a458249848501d047b6438
dulitharuvin <dulitha.ruvin@gmail.com>
2017-04-30 21:52:19 +0530

    Article view added to routing without login in

---
+++ client/config/ImageUpload.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Dulitha RD on 4/30/2017.
+ */
+$.cloudinary.config({
+    cloud_name: "hwshbhsyq"
+});
---
+++ server/config/ImageUpload.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Dulitha RD on 4/30/2017.
+ */
+Cloudinary.config({
+    cloud_name: 'hwshbhsyq',
+    api_key: '765487893716384',
+    api_secret: 'rYsjtZYYZGWa9hT_6mF-9BSZ36I'
+});
